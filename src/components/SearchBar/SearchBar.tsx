9a4acf56d58c014d054d365ad1ce2c2669fa0fca
Sathya Karunakalage <82775587+sathyaudayangi@users.noreply.github.com>
2023-08-10 15:46:52 +0530

    Add search box

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { Container, TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 export default function SearchBar() {
   return (
-      <Container maxWidth="md" sx={{ mt: 20 }}>
-        <TextField  type="
-        search" id="search" label="Search" sx={{ width: 600 }} />
-      </Container>
+    <div className="App">
+    <div className="knoware-title">
+      <h1>KnoWare</h1>
+    </div>
+    <Container className="search-container" maxWidth="md" sx={{ mt: 20 }}>
+      <Stack alignItems="center">
+        <Button className="upload-btn" variant="contained" component="label">
+          +
+          <input hidden accept="image/*" multiple type="file" />
+        </Button>
+      </Stack>
+      <TextField
+      className="search-field"
+        type="search"
+        id="search"
+        label="Search"
+        // sx={{ width: 500 }}
+      />
+    </Container>
+  </div>
   );
 }
