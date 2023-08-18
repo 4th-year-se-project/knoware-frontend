14462b41b3c969fbe9415233405c9707e3ed11cb
Sathya Karunakalage <82775587+sathyaudayangi@users.noreply.github.com>
2023-08-18 16:40:10 +0530

    Add left side bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LeftSideDrawer from "./components/Navigation/LeftSideDrawer";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <LeftSideDrawer />
+    </>
   );
 }
 
---
+++ src/components/Navigation/TopicList.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+    {
+        topic: 'Topic1',
+        subTopic: 'Subtopic1',
+    },
+    {
+        topic: 'Topic1',
+        subTopic: 'Subtopic1',
+    },
+];
