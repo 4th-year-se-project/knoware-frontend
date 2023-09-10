5a3b4ad7630e7fbba264d6e1420cf213be6facb9
Dulaj Prabasha <shad3s.of.m3@gmail.com>
2023-09-10 18:45:21 +0530

    add remaining endpoints to api file

--- student/src/services/embedAPI.ts
+++ student/src/services/embedAPI.ts
@@ -18,3 +18,83 @@ export const embedYoutube = async (
     throw error;
   }
 };
+
+export const embedPDF = async (file: File): Promise<AxiosResponse> => {
+  try {
+    const formData = new FormData();
+    formData.append("file", file);
+    const response = await axios.post(
+      "http://localhost:8080/embed_pdf",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    // Handle errors here, e.g., log them or show an error message to the user
+    throw error;
+  }
+};
+
+export const embedDoc = async (file: File): Promise<AxiosResponse> => {
+  try {
+    const formData = new FormData();
+    formData.append("file", file);
+    const response = await axios.post(
+      "http://localhost:8080/embed_docx",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    // Handle errors here, e.g., log them or show an error message to the user
+    throw error;
+  }
+};
+
+export const embedPPT = async (file: File): Promise<AxiosResponse> => {
+  try {
+    const formData = new FormData();
+    formData.append("file", file);
+    const response = await axios.post(
+      "http://localhost:8080/embed_pptx",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    // Handle errors here, e.g., log them or show an error message to the user
+    throw error;
+  }
+};
+
+export const embedAudio = async (file: File): Promise<AxiosResponse> => {
+  try {
+    const formData = new FormData();
+    formData.append("file", file);
+    const response = await axios.post(
+      "http://localhost:8080/embed_audio",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    // Handle errors here, e.g., log them or show an error message to the user
+    throw error;
+  }
+};
