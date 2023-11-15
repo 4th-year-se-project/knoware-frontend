296641ecd0c426e9b02db6014afaac280a8d9ec0
Sathya Karunakalage <82775587+sathyaudayangi@users.noreply.github.com>
2023-11-15 09:10:12 +0530

    Change mui icons

--- student/src/pages/Login.tsx
+++ student/src/pages/Login.tsx
@@ -8,11 +8,9 @@ import {
   FilledInput,
   InputAdornment,
   IconButton,
-  Typography
+  Typography,
 } from "@material-ui/core";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../login/authContext";
 
 const Login: React.FC = () => {
@@ -54,7 +52,7 @@ const Login: React.FC = () => {
       localStorage.setItem("access_token", accessToken);
       console.log("Login successful");
       login();
-      navigate("/home")
+      navigate("/home");
     } catch (error: any) {
       setError(error.message);
     }
@@ -92,7 +90,42 @@ const Login: React.FC = () => {
                   onMouseDown={handleMouseDownPassword}
                   edge="end"
                 >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                  {showPassword ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon icon-tabler icon-tabler-eye-off"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      stroke-width="2"
+                      stroke="currentColor"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M10.585 10.587a2 2 0 0 0 2.829 2.828" />
+                      <path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87" />
+                      <path d="M3 3l18 18" />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon icon-tabler icon-tabler-eye"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      stroke-width="2"
+                      stroke="currentColor"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                      <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+                    </svg>
+                  )}
                 </IconButton>
               </InputAdornment>
             }
