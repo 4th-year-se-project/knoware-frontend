11cb6431a1c2b06598ca2475df6b162466fec59a
Kalsha Samarajeewa <kalsha@Kalshas-MacBook-Air.local>
2023-12-08 16:02:49 +0530

    Remove unused module

--- student/src/components/HeaderBar.tsx
+++ student/src/components/HeaderBar.tsx
@@ -3,7 +3,6 @@ import Logo from "../assets/images/logo.svg";
 import DefaultAvatar from "../assets/images/avatar.jpg";
 import SearchBar from "./SearchBar";
 import { useSelector } from "react-redux";
-import axios from "axios";
 import { useAuth } from "../login/authContext";
 import { IconLogout } from '@tabler/icons-react';
 
