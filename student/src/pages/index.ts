6b538d15cc4b7aac68cca854793e794319dd195d
Yaiya Sam <72991092+YaiyaSam@users.noreply.github.com>
2024-01-01 22:21:57 +0530

    Add the lecturer dashboard (#34)
    * Add the lecturer dashboard
    
    * Change the fontsize in the table
    
    ---------
    
    Co-authored-by: Kalsha Samarajeewa <kalsha@Kalshas-MacBook-Air.local>

--- student/src/pages/index.ts
+++ student/src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as Home } from "./Home";
 export { default as ResourceHierarchy } from "./ResourceHierarchy";
 export { default as SearchResults } from "./SearchResults";
 export { default as NotFound } from "./NotFound";
+export { default as LecturerDashboard } from "./LecturerDashboard";
